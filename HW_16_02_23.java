eaa5e1a14ecc8836c5c458751e18d4967e8242f1
YefrAlex <117731121+YefrAlex@users.noreply.github.com>
2023-02-22 21:29:47 +0100

    Add files via upload

--- HW_16_02_23.java
+++ HW_16_02_23.java
@@ -13,6 +13,14 @@ public class HW_16_02_23 {
         int arr1[] = {11, 0, 1, 5, 4, 8, 9, 3, 2, 6, 12, 19, 7, -6, 50, 18, 14, 65, -37};
         int n = 13;
         int[] numsN = {1,3,9,11,13,17,4,5,3,2,17,4,1};
+        int[] arrmin = {4, 2, 3, 7, 65, 1, 12, 11, 10};
+        int[] fiboArr;
+        int lange = 7;
+        fiboArr = new int[lange];
+
+
+         Task_8.getMin(arrmin);
+        Task_9.getFibonachi(fiboArr);
         //Task_7.isDuplicated(numsN);
         //Task_6.getTwoNumbers(arr1, n);
         //Task_5.getMinSequence(numers1, n1);
@@ -192,6 +200,54 @@ class Task_7 {
                 j = i +1;
             }
 
+        }
+    }
+}
+class Task_8 {
+    public static void getMin(int[] arrmin) {
+        int min = arrmin[0];
+        int min2 = Integer.MIN_VALUE;
+        int min3 = Integer.MIN_VALUE;
+        int min4 = Integer.MIN_VALUE;
+
+        if (arrmin.length > 4) {
+            for (int i = 0; i < arrmin.length; i++) {
+                if (arrmin[i] < min) {
+                    min2 = min;
+                    min = arrmin[i];
+                } else if (arrmin[i] < min2) {
+                    min3 = min2;
+                    min2 = arrmin[i];
+                } else if (arrmin[i] < min3) {
+                    min4 = min3;
+                    min3 = arrmin[i];
+                } else if (arrmin[i] < min4) {
+                    min4 = arrmin[i];
+                }
+
+            }
+            System.out.println("MIN 1: " + min);
+            System.out.println("MIN 2: " + min2);
+            System.out.println("MIN 3: " + min3);
+            System.out.println("MIN 4: " + min4);
+        } else {
+            System.out.println("массив содержит 4 или  менее значений ");
+        }
+    }
+
+}
+class Task_9 {
+    // создать массив из чисел фибонач
+    public static void getFibonachi(int[] fiboAr) {
+        int lange = 11;
+        int[] fiboArr;
+        fiboArr = new int[lange];
+        for (int i = 0; i < lange; i++) {
+            if(i<2) {
+                fiboArr[i] = i;
+            } else
+                fiboArr[i] = fiboArr[i-2] + fiboArr[i-1];
+            System.out.println(fiboArr[i]);
         }
     }
 }
